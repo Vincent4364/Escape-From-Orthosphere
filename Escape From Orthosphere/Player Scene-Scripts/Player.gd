1f5aa4ebe973a000be252401eacace554963733c
Vincent4364 <21060@wc.school.nz>
2024-07-03 10:16:17 +1200

    changed headbob physics to animation + added sprint (subject to change)

--- Escape From Orthosphere/Player Scene-Scripts/Player.gd
+++ Escape From Orthosphere/Player Scene-Scripts/Player.gd
@@ -1,12 +1,14 @@
 extends CharacterBody3D
 
 #movement variables
-const SPEED = 5.0
+@export var speed = 5.0
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 8.0
 const JUMP_VELOCITY = 4.5
 const SENSITIVITY = 0.003
 
 #bob variables
-const BOB_FREQ = 2.0
+const BOB_FREQ = 10.0
 const BOB_AMP = 0.08
 var t_bob = 0.0
 
@@ -31,31 +33,41 @@ func _physics_process(delta):
 		
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
+	
+	var input_dir = Input.get_vector("Left","Right","Forward","Backwards")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if is_on_floor():
-		var input_dir = Input.get_vector("Left","Right","Forward","Backwards")
-		var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		if direction: 
-			velocity.x = direction.x * SPEED
-			velocity.z = direction.z * SPEED
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
 		else:
-			velocity.x = 0.0
-			velocity.z = 0.0
-	
-	t_bob = delta * velocity.length() * float(is_on_floor())
-	camera.transform.origin = _headbob(t_bob)
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+			
+		if input_dir.y > 0:
+			$Head/AnimationPlayer.play("bob")
+		elif input_dir.y < 0:
+			$Head/AnimationPlayer.play("bob")
+		elif input_dir.x > 0:
+			$Head/AnimationPlayer.play("bob")
+		elif input_dir.x < 0:
+			$Head/AnimationPlayer.play("bob")
+		else:
+			$Head/AnimationPlayer.stop()
 		
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+		
+
+func _process(delta):
 	move_and_slide()
-	
-func _headbob(time) -> Vector3:
-	var pos = Vector3.ZERO
-	pos.y = sin(time * BOB_FREQ) * BOB_AMP * 2
-	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
-	return pos
-
-
-
-
 
 
 #
