0556bcb5d959006af592b67175ca58acb8d861d6
Vincent4364 <21060@wc.school.nz>
2024-07-05 14:21:30 +1200

    added inertia

--- Escape From Orthosphere/Player Scene-Scripts/Player.gd
+++ Escape From Orthosphere/Player Scene-Scripts/Player.gd
@@ -7,13 +7,9 @@ const SPRINT_SPEED = 8.0
 const JUMP_VELOCITY = 4.5
 const SENSITIVITY = 0.003
 
-#bob variables
-const BOB_FREQ = 10.0
-const BOB_AMP = 0.08
-var t_bob = 0.0
-
 #physics variables
 var gravity = 9.8
+var can_move = true
 
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
@@ -28,6 +24,7 @@ func _unhandled_input(event):
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40,), deg_to_rad(60))
 
 func _physics_process(delta):
+	move_and_slide()
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 		
@@ -38,11 +35,12 @@ func _physics_process(delta):
 		speed = SPRINT_SPEED
 	else:
 		speed = WALK_SPEED
-	
+		
 	var input_dir = Input.get_vector("Left","Right","Forward","Backwards")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if is_on_floor():
+		print(float(is_on_floor()))
 		if direction: 
 			velocity.x = direction.x * speed
 			velocity.z = direction.z * speed
@@ -64,10 +62,7 @@ func _physics_process(delta):
 	else:
 		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
-		
 
-func _process(delta):
-	move_and_slide()
 
 
 #
