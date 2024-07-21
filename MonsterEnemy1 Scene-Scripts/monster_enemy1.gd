73b8b6f33018d1d766a9db2c1bfa0247f98e7b4c
Vincent4364 <103389512+Vincent4364@users.noreply.github.com>
2024-07-21 02:39:43 +0100

    test

--- MonsterEnemy1 Scene-Scripts/monster_enemy1.gd
+++ MonsterEnemy1 Scene-Scripts/monster_enemy1.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @onready var anim_player = $AnimationPlayer
 
+@export var health = 100
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	anim_player.play("Walk")
@@ -10,3 +12,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func hit():
+	if health > 0: 
+		health =- 10
+	else:
+		queue_free()
